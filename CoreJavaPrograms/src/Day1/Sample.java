38bad70182f8d6c92e78ff0d482e05b220fd9d40
Manimehalai-S <agimegha982@gmail.com>
2025-09-24 15:00:48 +0530

    This project is my example practices of core java

---
+++ CoreJavaPrograms/src/Day1/Sample.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class Sample {
+
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
