48cecdd7570da706e7ce35329dd4abe13d913621
Manimehalai-S <agimegha982@gmail.com>
2025-09-30 07:16:55 +0530

    This is sample java program

--- CoreJavaPrograms/src/Day1/Sample.java
+++ CoreJavaPrograms/src/Day1/Sample.java
@@ -3,11 +3,11 @@ package Day1;
 public class Sample {
 
 	public Sample() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.print("Welcome");
 
 	}
 
