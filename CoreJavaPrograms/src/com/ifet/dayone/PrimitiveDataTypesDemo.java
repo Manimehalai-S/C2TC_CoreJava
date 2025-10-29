1cbd53423fc0b9041676fac89b41c5568b028f9a
Manimehalai-S <agimegha982@gmail.com>
2025-10-29 21:30:48 +0530

    Day one of my java program

--- CoreJavaPrograms/src/com/ifet/dayone/PrimitiveDataTypesDemo.java
+++ CoreJavaPrograms/src/com/ifet/dayone/PrimitiveDataTypesDemo.java
@@ -1,5 +1,23 @@
 package com.ifet.dayone;
 
 public class PrimitiveDataTypesDemo {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 20000;
+        int i = 500000;
+        long l = 15000000000L;
+        float f = 3.14f;
+        double d = 99.99;
+        char c = 'A';
+        boolean flag = true;
 
+        System.out.println("byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+        System.out.println("char value: " + c);
+        System.out.println("boolean value: " + flag);
+    }
 }
